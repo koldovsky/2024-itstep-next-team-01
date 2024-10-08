80387d9db9d1eb184c0416a99799f931d15d35b2
Misha376 <iskivmisha@gmail.com>
2024-10-08 23:09:40 +0100

    header created

---
+++ flower-shop/app/components/layout/header/Navigation.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+type NavigationProps = {
+  isDesktop: boolean;
+};
+
+export default function Navigation({ isDesktop }: NavigationProps) {
+  return (
+    <nav className={`${isDesktop ? 'hidden md:flex flex-row space-x-8 text-sm font-normal' : 'text-center text-sm font-normal'}`}>
+      {[
+        { href: '/about', label: 'ABOUT US' },
+        { href: '/services', label: 'SERVICES' },
+        { href: '/flowers', label: 'FLOWERS' },
+        { href: '/testimonials', label: 'TESTIMONIALS' },
+        { href: '/contacts', label: 'CONTACTS' },
+      ].map(({ href, label }) => (
+        <Link key={href} href={href} passHref>
+          <span className="hover:text-hoverText cursor-pointer block">{label}</span>
+        </Link>
+      ))}
+    </nav>
+  );
+}
