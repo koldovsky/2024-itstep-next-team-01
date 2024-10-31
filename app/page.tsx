40cad8b9d88b55f80e9ab81d33bda904300d5875
Dmitry-Savch <savchdimon@gmail.com>
2024-10-31 16:43:10 +0200

    finished master classes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import NewInShop from "./components/Home/NewInShop";
 import Testimonials from "./components/Home/Testimonials";
 import Instagram from "./components/Home/Instagram";
 import ServicesSection from "./components/Home/ServiceSection/ServicesSection";
-import MasterClassesSection from "./components/Home/MasterClasses/MasterClassesSection";
+import MasterClassesSection from "@/app/components/AboutUs/MasterClasses/MasterClassesSection";
 import PageTitle from "@/app/components/Home/AboutUsTitle/PageTitle";
 
 export default function Home() {
