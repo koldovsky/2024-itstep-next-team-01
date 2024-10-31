611c7a36101b4f1a87362ae8d84ca5052d36f686
Misha376 <iskivmisha@gmail.com>
2024-10-31 16:27:43 +0000

    fixed apos

--- app/about-us/MasterClasses/MasterClassesTitle.tsx
+++ app/about-us/MasterClasses/MasterClassesTitle.tsx
@@ -3,7 +3,7 @@ export default function MasterClassesTitle(    {}  ) {
     return (
         <div>
             <p className="text-specialText font-mrsSaint text-3xl mb-6">Master classes</p>
-            <h1 className="font-frank text-5xl text-primaryText">Registration for Master Classes in Sami's Flowers</h1>
+            <h1 className="font-frank text-5xl text-primaryText">Registration for Master Classes in Sami&apos;s Flowers</h1>
             <hr className='text-hrColor w-14 border-2 my-3'/>
             <p className="font-inter text-xl text-secondaryText mb-7">Want to learn how to create bouquets yourself?</p>
             <p className="font-inter text-secondaryText">From time to time, we hold master classes for everyone who wants to learn how to work with flowers and create unique bouquets.</p>
--- app/components/Home/Gallery/DescriptionInfo.tsx
+++ app/components/Home/Gallery/DescriptionInfo.tsx
@@ -6,7 +6,7 @@ export default function DescriptionInfo() {
             <div className="w-full md:w-2/3 md:pr-8 mb-6 md:mb-0">
                 <p className="mb-4 text-base sm:text-lg text-contentText">
                     Buying flowers for your loved ones is an important way to show them that <u>you care.</u> Flowers have a
-                    powerful emotional impact, conveying love, sympathy, and other heartfelt sentiments. Giving flowers is a gesture of kindness that can make a difference in someone's life and make them feel special.
+                    powerful emotional impact, conveying love, sympathy, and other heartfelt sentiments. Giving flowers is a gesture of kindness that can make a difference in someone&apos;s life and make them feel special.
                 </p>
             </div>
             <div className="w-full md:w-1/3">
